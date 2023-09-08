920b140538e343a1e9241b1af97af092ab4afa8e
LudoPilot <e.berardfr@gmail.com>
2023-09-08 20:10:51 +0200

    Score added.

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ document.addEventListener('DOMContentLoaded', () => {
 	const resultDisplay = document.getElementById('result')
 	const width = 4
 	let squares = []
+	let score = 0
 
 	// Create the board
 	function createBoard() {
@@ -119,6 +120,8 @@ document.addEventListener('DOMContentLoaded', () => {
 				let combinedTotal = parseInt(squares[i].innerHTML + squares) + parseInt(squares[i+1].innerHTML)
 				squares[i].innerHTML = combinedTotal
 				squares[i+1].innerHTML = 0
+				score += combinedTotal
+				scoreDisplay.innerHTML = score
 			}			
 		}
 		checkForWin()
@@ -130,6 +133,8 @@ document.addEventListener('DOMContentLoaded', () => {
 				let combinedTotal = parseInt(squares[i].innerHTML + squares) + parseInt(squares[i+width].innerHTML)
 				squares[i].innerHTML = combinedTotal
 				squares[i+width].innerHTML = 0
+				score += combinedTotal
+				scoreDisplay.innerHTML = score
 			}			
 		}
 		checkForWin()
@@ -195,7 +200,7 @@ document.addEventListener('DOMContentLoaded', () => {
 		}
 		if (zeros === 0) {
 			resultDisplay.innerHTML = 'Game over'
-			document.removeEventListener('key', control)
+			document.removeEventListener('keyup', control)
 		}
 	}
 })
