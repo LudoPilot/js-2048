2b3ca1877b2acb63c81e6ec4f8af07fa8c2d555c
LudoPilot <e.berardfr@gmail.com>
2023-09-08 17:44:39 +0200

    Random number generator added. Function added to group the tiles 4 by 4.

--- app.js
+++ app.js
@@ -13,6 +13,32 @@ document.addEventListener('DOMContentLoaded', () => {
 			gridDisplay.appendChild(square)
 			squares.push(square)
 		}
+		generate()
+		generate()
 	}
 	createBoard()
+
+	// Generate a number for
+	function generate() {
+		let randomNumber = Math.floor(Math.random() * squares.length)
+		if (squares[randomNumber].innerHTML == 0) {
+			squares[randomNumber].innerHTML = 2
+		} else generate()
+	}
+
+	// group the tiles 4 by 4 
+	function moveRight() {
+		for (let i = 0; i < 16; i++) {
+			if (i % 4 === 0) {
+				let totalOne = squares[i].innerHTML
+				let totalTwo = squares[i+1].innerHTML
+				let totalThree = squares[i+2].innerHTML
+				let totalFour = squares[i+3].innerHTML
+				let row = [parseInt(totalOne), parseInt(totalTwo), parseInt(totalThree), parseInt(totalFour)]
+
+				console.log(row)
+			}
+		}
+	}
+	moveRight()
 })
